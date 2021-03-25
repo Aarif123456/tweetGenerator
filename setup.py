557830c0013a9e11025127141690546344992067
Abdullah Arif <abdullahmeo11@gmail.com>
2021-03-25 00:24:55 -0400

    changed package url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A bot that can take tweets from twitter and generate twitter that mimic the user",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/aarif123456/",
+    url="https://github.com/Aarif123456/tweetGenerator",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
